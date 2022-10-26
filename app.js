58f90fba534ea227442d39b28f3e6d8dde0378aa
Shashwat <shashwatvirat5127@gmail.com>
2022-10-26 19:25:09 +0530

    added operator choice with console relations

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let a=prompt("enter first num");
 let b=prompt("enter second num");
+let c=prompt("choose and operation. + - * /");
+let opertator= c;
 num1 = parseInt(a);
 num2 = parseInt(b);
 
@@ -32,7 +34,25 @@ function divide(num1,num2){
 
 
 
-add(num1,num2);
-subtract(num1,num2);
-multiply(num1,num2);
-divide(num1,num2);
+function operate(operator,num1,num2){
+    switch(operator){
+        case "+":
+            add(num1,num2);
+            console.log("using operator", operator);
+            break;
+            case "-":
+                subtract(num1,num2)
+                console.log("using operator", operator);
+                break;
+                case "*":
+                multiply(num1,num2)
+                console.log("using operator", operator);
+                break;
+                case "/":
+                divide(num1,num2)
+                console.log("using operator", operator);
+                break;
+        }
+    }
+    
+    operate(opertator,num1,num2);
